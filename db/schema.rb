b6cb57371414c3f13d7a68537346d6e7574a888f
Alex Vlasov <drumlife182@gmail.com>
2022-10-08 14:55:48 +0200

    Added migrations and models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_155644) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_08_120404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "city"
+    t.string "user_name"
+    t.string "password"
+    t.string "address"
+    t.string "primary_practice"
+    t.string "secondary_practice"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
