b33bc504d59ac881ccb2c8af2616a45135bead19
Alex Vlasov <drumlife182@gmail.com>
2022-10-14 11:29:18 +0200

    Added doctors_controller && view index, show, new templates

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,36 @@
 class DoctorsController < ApplicationController
+  before_action :set_doctor, only: %i[show destroy]
+  def index
+    @doctors = Doctor.all
+  end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      redirect_to(doctors_path)
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def destroy
+    @doctor.destroy
+    redirect_to(doctors_path)
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:first_name, :last_name, :city, :user_name, :password, :address, :primary_practice, :secondary_practice)
+  end
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
 end
