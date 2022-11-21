547db3633d6a31670f775bf8f698e430fe0f43af
Alex Vlasov <drumlife182@gmail.com>
2022-11-21 20:38:17 +0100

    Fixed all the code in the application

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,15 +4,17 @@ CarrierWave.configure do |config|
   config.aws_acl    = 'private'
   config.asset_host = 'http://example.com'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
-  config.aws_attributes = -> { {
+  config.aws_attributes = lambda {
+    {
       expires: 1.week.from_now.httpdate,
-      cache_control: 'max-age=604800'
-  } }
+      cache_control: 'max-age=604800',
+    }
+  }
 
   config.aws_credentials = {
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      region:            ENV.fetch('AWS_REGION'), # Required
-      stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region: ENV.fetch('AWS_REGION'), # Required
+    stub_responses: Rails.env.test?, # Optional, avoid hitting S3 actual during tests
   }
 end
